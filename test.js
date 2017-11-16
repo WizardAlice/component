971e66690c63d5002dbaa56bfdbeff5d4d5d628e
WizardAlice <saberbk@outlook.com>
2017-11-16 10:13:56 +0800

    测试了一个promise.race方法

--- test.js
+++ test.js
@@ -19,17 +19,75 @@
 // fil("alice eric 123", "alice_eric_paradiser") 返回false，因为不包含123
 // fil("alice eric", "alice_eric_paradiser") 返回true
 
-function compact(arr){
-  let res = []
-  arr.map((v) => {
-    if(!res.includes(v)){
-      res.push(v)
-    }
-  })
-  return res
-}
 
-function getFrequest(str){
-  let arr = str.split("")
-  arr = compact(arr)
-}
+
+
+
+// function compact(arr){
+//   let res = []
+//   arr.map((v) => {
+//     if(!res.includes(v)){
+//       res.push(v)
+//     }
+//   })
+//   return res
+// }
+
+// function getFrequest(str){
+//   let arr = str.split("")
+//   arr = compact(arr)
+// }
+
+
+
+let a = new Promise((resolve, reject) =>{
+  setTimeout(resolve, 10, "大的")
+})
+
+let b = new Promise((resolve, reject) =>{
+  resolve("小的")
+})
+
+Promise.race([a, b]).then((res) => {
+  console.log(res)
+})
+
+// var p1 = new Promise(function(resolve, reject) { 
+//     setTimeout(resolve, 500, "one"); 
+// });
+
+// var p2 = new Promise(function(resolve, reject) { 
+//      
+// });
+
+// Promise.race([p1, p2]).then(function(value) {
+//   console.log(value); // "two"
+//   // 两个都完成，但 p2 更快
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
