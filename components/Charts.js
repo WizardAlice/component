2fc879f4bef5fbeadb12f7f498b788e4bde2258f
WizardAlice <saberbk@outlook.com>
2017-11-23 15:17:44 +0800

    怕丢了临时存一份

--- components/Charts.js
+++ components/Charts.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import ReactEcharts from 'echarts-for-react'
 import echarts from 'echarts'
+import { chartOption } from '../controller/method'
 
 echarts.registerTheme('my_theme', {
   legend: {
@@ -15,211 +16,27 @@ echarts.registerTheme('my_theme', {
 export default class Allbook extends Component{
   constructor(props, context){
     super(props, context)
-    if(this.props.ratio){
-      this.state = {
-        option: {
-          tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            },
-            formatter: (params) => {
-              let str = params[0].name.toString()
-              params.map((v) => {
-                str += "<br>"
-                str += '<span style="display:inline-block;margin-left:5px;margin-right:5px;';
-                str += 'border-radius:10px;width:9px;height:9px;';
-                str += 'background-color:'+ v.color +'"></span>';
-                str += v.seriesName.toString()
-                str += " : "
-                str += (v.value*100).toFixed(2)
-                str += "%"
-              })
-              return str
-            }
-          },
-          legend: {
-            data: this.props.data[0].legend.map((v)=> v.toString()),
-            bottom: "80%",
-            width: "80%"
-          },
-          toolbox: {
-            feature: {
-                saveAsImage: {}
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis : [
-            {
-              type : 'category',
-              boundaryGap : this.props.data[1].boundaryGap,
-              data : this.props.data[1].data
-            }
-          ],
-          yAxis : [
-            {
-              type : this.props.data[2].yAxis,
-              axisLabel: {
-                formatter: (value, index) => {
-                  return (value * 100).toFixed(2)  + '%'
-                }
-              }          
-            }
-          ],
-          series : this.props.data[2].data.map((v) => Object.assign({}, v))
-        }
-      }
-    }
-    else{
-      this.state = {
-        option: {
-          tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            }
-          },
-          legend: {
-            data: this.props.data[0].legend.map((v)=> v.toString()),
-            bottom: "80%",
-            width: "80%"
-          },
-          toolbox: {
-            feature: {
-                saveAsImage: {}
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis : [
-            {
-              type : 'category',
-              boundaryGap : this.props.data[1].boundaryGap,
-              data : this.props.data[1].data
-            }
-          ],
-          yAxis : [
-            {
-              type : this.props.data[2].yAxis
-            }
-          ],
-          series : this.props.data[2].data.map((v) => Object.assign({}, v))
-        }
-      }
+    this.state = {
+      option: chartOption({
+        ratio: this.props.ratio,
+        legendDate: this.props.data[0].legend,
+        x: this.props.data[1],
+        y: this.props.data[2].yAxis,
+        seriesData: this.props.data[2].data
+      })
     }
   }
   componentWillReceiveProps(newxProps) {
     // let echarts_instance = this.echarts_react.getEchartsInstance()
-    if(newxProps.ratio){
-      this.setState({
-        option: {
-          tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            },
-            formatter: (params) => {
-              let str = params[0].name.toString()
-              params.map((v) => {
-                str += "<br>"
-                str += '<span style="display:inline-block;margin-left:5px;margin-right:5px;';
-                str += 'border-radius:10px;width:9px;height:9px;';
-                str += 'background-color:'+ v.color +'"></span>';
-                str += v.seriesName.toString()
-                str += " : "
-                str += (v.value*100).toFixed(2)
-                str += "%"
-              })
-              return str
-            }
-          },
-          legend: {
-            data: newxProps.data[0].legend.map((v)=> v.toString()),
-            bottom: "80%",
-            width: "80%"
-          },
-          toolbox: {
-            feature: {
-                saveAsImage: {}
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis : [
-            {
-              type : 'category',
-              boundaryGap : newxProps.data[1].boundaryGap,
-              data : newxProps.data[1].data
-            }
-          ],
-          yAxis : [
-            {
-              type : newxProps.data[2].yAxis,
-              axisLabel: {
-                formatter: (value, index) => {
-                  return (value * 100).toFixed(2)  + '%'
-                }
-              }          
-            }
-          ],
-          series : newxProps.data[2].data.map((v) => Object.assign({}, v))
-        }
+    this.setState({
+      option: chartOption({
+        ratio: newxProps.ratio,
+        legendDate: newxProps.data[0].legend,
+        x: newxProps.data[1],
+        y: newxProps.data[2].yAxis,
+        seriesData: newxProps.data[2].data
       })
-    }
-    else{
-      this.setState({
-        option: {
-          tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            }
-          },
-          legend: {
-            data: newxProps.data[0].legend.map((v)=> v.toString()),
-            bottom: "80%",
-            width: "80%"
-          },
-          toolbox: {
-            feature: {
-                saveAsImage: {}
-            }
-          },
-          grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-          },
-          xAxis : [
-            {
-              type : 'category',
-              boundaryGap : newxProps.data[1].boundaryGap,
-              data : newxProps.data[1].data
-            }
-          ],
-          yAxis : [
-            {
-              type : newxProps.data[2].yAxis
-            }
-          ],
-          series : newxProps.data[2].data.map((v) => Object.assign({}, v))
-        }
-      })
-    }
+    })
     // echarts_instance.clear()
   }
   shouldComponentUpdate(nextProps, nextState){
--- controller/fetchApi.js
+++ controller/fetchApi.js
@@ -23,7 +23,7 @@ export function gettable(){
 }
 
 // let url_head = "https://themis.didiman.com/"
-let url_head = ""
+let url_head = "http://localhost:3000/"
 
 export function getForm(str){
   // let str = getUrl(window.location.href)
