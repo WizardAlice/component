6c46600eb7b8c3176ec1558413f965ea69a02e85
WizardAlice <saberbk@outlook.com>
2017-11-24 10:16:56 +0800

    将图的代码机构进行了优化

--- controller/method.js
+++ controller/method.js
@@ -71,3 +71,154 @@ export function removeByValue(arr, val) {
   }
   return arr
 }
+
+export function labelFormatter(params){
+  if(params.value >500){
+    return params.value
+  }else{
+    return ""
+  }
+}
+
+export function labelFormatterRatio(params){
+  if(params.value >0.05){
+    return (params.value*100).toFixed(2).toString() + "%"
+  }else{
+    return ""
+  }
+}
+
+export function chartOption({ratio = false, legendDate, x, y, seriesData}){
+  let tooltip = {
+    trigger: 'axis',
+    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+    }
+  }
+  if(ratio === true){
+    tooltip = Object.assign(tooltip, {
+      formatter: (params) => {
+        let str = params[0].name.toString()
+        params.map((v) => {
+          str += "<br>"
+          str += '<span style="display:inline-block;margin-left:5px;margin-right:5px;';
+          str += 'border-radius:10px;width:9px;height:9px;';
+          str += 'background-color:'+ v.color +'"></span>';
+          str += v.seriesName.toString()
+          str += " : "
+          if(v.value > 10){
+            str += v.value
+          }else{
+            str += (v.value*100).toFixed(2) + "%"
+          }
+        })
+        return str
+      }
+    })
+  }
+
+  let legend = {
+    data : legendDate.map((v) => v.toString()),
+    bottom: "80%",
+    width: "80%"
+  }
+
+  let toolbox = {
+    feature: {
+      saveAsImage: {}
+    }
+  }
+  let grid = {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  }
+  let xAxis = {
+    type : 'category',
+    boundaryGap : x.boundaryGap,
+    data : x.data
+  }
+
+  let yAxis = y.map((v) => {
+    if (v.type == "value"){
+      return {
+        type : "value",
+        axisLabel: {
+          formatter: (value, index) => {
+            if(ratio === true){
+              return (value * 100).toFixed(2)  + '%'
+            }else{
+              return value
+            }
+          }
+        }
+      }
+    }else if (v.type == 'right'){
+      return { type: 'value' , name: v.name,
+        axisLabel: {
+        }
+      }
+    }
+  })
+
+  let series = seriesData.map((v) => {
+    if(v.type == "bar"){
+      if(ratio === true){
+        return Object.assign({}, v, {
+          label: {
+            normal: {
+              show: true,
+              position: 'inside',
+              formatter: labelFormatterRatio
+            }
+          }
+        })
+      }else{
+        return Object.assign({}, v, {
+          label: {
+            normal: {
+              show: true,
+              position: 'inside',
+              formatter: labelFormatter
+            }
+          }
+        })
+      }
+    }else{
+      return Object.assign({}, v)
+    }
+  })
+  return {
+    tooltip: tooltip,
+    legend: legend,
+    toolbox: toolbox,
+    grid: grid,
+    xAxis: xAxis,
+    yAxis: yAxis,
+    series: series
+  }
+}
+
+export function toPercentsFormatter(type, value, index){
+  if (type == "ratio"){   //在于使用了百分比时的y轴
+    return (value * 100).toFixed(2)  + '%'
+  }else if (type == "basic"){    //在于普通的y轴
+    return value
+  }
+}
+
+export function handleBarMark(params){
+  if(params.value >500){
+    return params.value
+  }else{
+    return ""
+  }
+}
+
+
+
+
+
+
+
