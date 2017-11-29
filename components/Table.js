2f3ebe8f7f19283763e68cf2ea7b6e45300deef5
WizardAlice <saberbk@outlook.com>
2017-11-29 11:16:16 +0800

    对于小于零的数不显示，但是对于没有extra的数，不做处理

--- components/Table.js
+++ components/Table.js
@@ -6,15 +6,21 @@ import cx from 'classnames'
 
 
 function getPercents(num) {
+  if(num < 0){
+    return '0'
+  }
   return (Math.round(num * 10000)/100).toFixed(2) + '%';
 }
 
 function getThousands(num) {
   var number = new Number(num);
-    var str = number.toString();
-    var newstr = str.replace(/\d{1,3}(?=(\d{3})+$)/g,function(s){
-        return s+','
-    })
+  if(number < 0){
+    return '0'
+  }
+  var str = number.toString();
+  var newstr = str.replace(/\d{1,3}(?=(\d{3})+$)/g,function(s){
+      return s+','
+  })
   return newstr;
 }
 export default class Table extends Component{
